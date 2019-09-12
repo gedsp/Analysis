30a8b4be3dbb77d552f615c594c374f562d50dfb
Gloria de Sa Pereira <gloria@desapereira.com>
2019-09-12 17:40:25 +0100

    maa

--- CRT_ana/mana.cpp
+++ CRT_ana/mana.cpp
@@ -24,11 +24,18 @@ void mana(){
 
 //  hnseg = new TH1F("hnseg","Number of segments for selected tracks",4096,0,8192);
   if (M.fChain == 0) return;
-
+  M.Init(M.fChain);
   Long64_t nentries = M.fChain->GetEntriesFast();
 
+  
+  
   Long64_t nbytes = 0, nb = 0;
-
+  TH1F* plano0 = new TH1F("plano0", "plano0", 10000,1,4100);
+  TH1F* plano1 = new TH1F("plano1", "plano1", 10000,1,4100);
+  TH1F* plano2 = new TH1F("plano2", "plano2", 10000,1,4100);
+  TH1F* plano3 = new TH1F("plano3", "plano3", 10000,1,4100);
+  
+  
   for (Long64_t jentry=0; jentry<nentries;jentry++) {
  
     Long64_t ientry = M.LoadTree(jentry);
@@ -37,34 +44,31 @@ void mana(){
   
     nb = M.fChain->GetEntry(jentry);   nbytes += nb;
   // if (Cut(ientry) < 0) continue;
+   // hist->Fill(crt_adc);
 
+    
+      for (int j=0; j<32; j++) {
+        plano0->Fill(crt_adc[0][j]);
+        plano1->Fill(crt_adc[1][j]);
+        plano2->Fill(crt_adc[2][j]);
+        plano3->Fill(crt_adc[3][j]);
+      }
     
     
   }
   
-TCanvas * c =new TCanvas();
-c->Divide(2,2);
-c->cd(1);
-  md->Draw("crt_charge");
-c->cd(2);
-md->Draw("crt_nHitAllTot");
-c->cd(3);
-md->Draw("crt_nHitTot");
-c->cd(4);
-md->Draw("crt_nHitSatTot");
-
-  c->Print("Crt_CH_Histtot.pdf");
-  
-  for (int i=0; i<4; i++) {
-    TCanvas * c2 = new TCanvas();
-    c2->Divide(8,4);
+ 
+ 
+  TCanvas * c2 = new TCanvas();
+  c2->Divide(2,2);
     
-    for (int j=0; j<32; j++) {
-      c2->cd(j+1);
-      md->Draw("crt_adc[i][j]");
-    }
-    c2->Print(Form("Crt_adc_%d",i));
-  }
+  plano0->Draw("hist");
+  plano1->Draw("hist");
+  plano2->Draw("hist");
+  plano3->Draw("hist");
+    
+  c2->Print("Crt_adc_.pdf");
+  
 
   //md->Draw("crt_adc");
 
