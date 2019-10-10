3dd9ace035291cfcbba3b8e6e13b2dd32496b05a
Gloria de Sa Pereira <gloria@desapereira.com>
2019-10-10 23:21:45 +0100

    new plots

--- CRT_ana/mana.cpp
+++ CRT_ana/mana.cpp
@@ -741,8 +741,8 @@ void mana(){
   cut.close();
   
   
-  Canvas->Print("local/Track_z_ys.pdf");
-  Canvas2->Print("local/Track_x_ys.pdf");
+  Canvas->Print("lxplus/Track_z_ys.pdf");
+  Canvas2->Print("lxplus/Track_x_ys.pdf");
   
   double xxtot=xf12+xf14+xf23+xf34;
   double zztot=zf14+zf23+zf34;
@@ -800,7 +800,7 @@ gStyle->SetGridStyle(3);
      plano_cut_hprof[3]->SetLineWidth(3);
   plano_cut_hprof[3]->Draw("E");
 
-    c1->Print("local/tprofile.pdf");
+    c1->Print("lxplus/tprofile.pdf");
   
   
 */
@@ -870,7 +870,7 @@ gStyle->SetGridStyle(3);
   V_hist[1]->Fit(g7,"R");
   V_hist[1]->Fit(g8,"R+");
     
-  c2->Print("local/Crt_adc_diff.pdf");
+  c2->Print("lxplus/Crt_adc_diff.pdf");
  */
  /**************************************************/
  
@@ -883,7 +883,7 @@ gStyle->SetGridStyle(3);
   c3->cd(2);
   plano23->Draw("COLZ");
 
-  c3->Print("local/Crt_coincidencias_barras.pdf");
+  c3->Print("lxplus/Crt_coincidencias_barras.pdf");
 
 
   /**************************************************/
@@ -891,7 +891,7 @@ gStyle->SetGridStyle(3);
   
   TCanvas * c7 = new TCanvas();
   tof->Draw("hist");
-  c7->Print("local/tof.pdf");
+  c7->Print("lxplus/tof.pdf");
   
    /**************************************************/
 
@@ -904,7 +904,7 @@ gStyle->SetGridStyle(3);
   c8->cd(3);
   disp23->Draw("hist");
   
-   c8->Print("local/disp.pdf");
+   c8->Print("lxplus/disp.pdf");
    
     /**************************************************/
   
@@ -931,7 +931,7 @@ gStyle->SetGridStyle(3);
    legend1->AddEntry(V_bars_cut[3],"Cut","l");
  //  legend1->Draw();
   
-   c12->Print("local/barswevents_precut.pdf");
+   c12->Print("lxplus/barswevents_precut.pdf");
    
     /**************************************************/
   
@@ -947,7 +947,7 @@ gStyle->SetGridStyle(3);
      c13->cd(4);
      V_delta_max[3]->Draw("hist");
   
-    c13->Print("local/deltaSB.pdf");
+    c13->Print("lxplus/deltaSB.pdf");
     
   /**************************************************/
   
@@ -963,7 +963,7 @@ gStyle->SetGridStyle(3);
      c15->cd(4);
      V_delta_max_2[3]->Draw("COLZ");
   
-    c15->Print("local/deltaSB_EnMAx.pdf");
+    c15->Print("lxplus/deltaSB_EnMAx.pdf");
   
   
      /**************************************************/
@@ -984,14 +984,14 @@ gStyle->SetGridStyle(3);
    legend->AddEntry(coor_costhetatof_m0,"TOF<0","l");
     legend->AddEntry(coor_costheta,"TOF>0","l");
     legend->Draw();
-   c9->Print("local/costheta.pdf");
+   c9->Print("lxplus/costheta.pdf");
    
   
    /**************************************************/
 
    TCanvas * c10 = new TCanvas();
    LvsTOF->Draw("COLZ");
-   c10->Print("local/LVSTOF.pdf");
+   c10->Print("lxplus/LVSTOF.pdf");
    
 
   
@@ -1000,7 +1000,7 @@ gStyle->SetGridStyle(3);
 
    TCanvas * c11 = new TCanvas();
    costhetavsphi->Draw("COLZ");
-   c11->Print("local/costhetavsphi.pdf");
+   c11->Print("lxplus/costhetavsphi.pdf");
    
 
   
@@ -1009,7 +1009,7 @@ gStyle->SetGridStyle(3);
   
    TCanvas * c14 = new TCanvas();
    pmt->Draw("hist");
-   c14->Print("local/pmt.pdf");
+   c14->Print("lxplus/pmt.pdf");
    
 
   
@@ -1027,7 +1027,7 @@ gStyle->SetGridStyle(3);
   plano2W->Draw("COLZ");
   c31->cd(4);
   plano3W->Draw("COLZ");
-  c31->Print("local/Crt_barras_weighted.pdf");
+  c31->Print("lxplus/Crt_barras_weighted.pdf");
 
 */
 
@@ -1052,7 +1052,7 @@ gStyle->SetGridStyle(3);
   V_plano_tot_cut[2]->Draw("hist");
   c4->cd(4);
    V_plano_tot_cut[3]->Draw("hist");
-  c4->Print("local/Crt_V_plano_tot_cut[0].pdf");
+  c4->Print("lxplus/Crt_V_plano_tot_cut[0].pdf");
 
  
  
@@ -1071,7 +1071,7 @@ gStyle->SetGridStyle(3);
     V_1maxvs2max[2]->Draw("COLZ");
    c5->cd(4);
     V_1maxvs2max[3]->Draw("COLZ");
-   c5->Print("local/1stmaxvs2ndmax.pdf");
+   c5->Print("lxplus/1stmaxvs2ndmax.pdf");
 
 
 
